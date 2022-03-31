87d3d4814e0bdc60571038a1aec72ce3dfdd7664
TeemuL <teemu.s.laine@tuni.fi>
2022-03-31 23:53:16 +0300

    Added Java folder

---
+++ Java/testing.java
@@ -0,0 +1,10 @@
+class testing {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("bruh");
+            if (i == 2) {
+                System.out.println("third bruh");
+            }
+        }
+    }
+}
